25bea899e5d0964ad565fd7ecc5c54b34e9912e2
Jacob Gillespie <me@jacobwg.com>
2012-02-19 13:12:51 -0600

    rails 3.2.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InformedCinema::Application.config.secret_token = 'b2869929c7872b0d687bb7a90f41141500ccd22130eceb9b51239a451cf518933e3081e1fa6ccb6d9e1781fe3fe9016a2059ac738482f1ba2531036c12fe0635'
