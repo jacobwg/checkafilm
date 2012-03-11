a8a5e9eed2d963ba915d9fab6525c4b8da75dd80
Jacob Gillespie <me@jacobwg.com>
2012-03-10 21:36:20 -0600

    added heroku settings

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,5 +1,13 @@
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
+  if ENV['HEROKU_SETTINGS'] == "yes"
+    source "#{Rails.root}/config/application.heroku.yml"
+  else
+    if FileTest.exists?("#{Rails.root}/config/application.yml")
+      source "#{Rails.root}/config/application.yml"
+    else
+      source "#{Rails.root}/config/application.sample.yml"
+    end
+  end
   namespace Rails.env
 end
 
