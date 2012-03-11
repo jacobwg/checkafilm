dc682fe9691a798cd8d8347abd51e3d38f8bfab6
Jacob Gillespie <me@jacobwg.com>
2012-03-10 23:40:15 -0600

    fix

--- config/initializers/carrierswave.rb
+++ config/initializers/carrierswave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => Settings.aws_key,
     :aws_secret_access_key  => Settings.aws_secret,
-    :region                 => 'eu-west-1'
+#    :region                 => 'eu-west-1'
   }
   config.fog_directory  = 'jacobwg'                     # required
 #  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
