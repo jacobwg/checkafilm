9f39140426800e68b7ea58189e9b5f0341ae24f7
Jacob Gillespie <me@jacobwg.com>
2012-03-10 23:36:32 -0600

    bucket change

--- config/initializers/carrierswave.rb
+++ config/initializers/carrierswave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => Settings.aws_secret,
     :region                 => 'eu-west-1'
   }
-  config.fog_directory  = 'informed-cinema'                     # required
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  config.fog_directory  = 'jacobwg'                     # required
+#  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
