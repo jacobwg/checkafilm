a0186fd6a129094c91583bbbfa67b8cb1fc67a4f
Jacob Gillespie <me@jacobwg.com>
2012-03-12 18:51:43 -0500

    new search page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,31 @@ class MoviesController < ApplicationController
   end
 
   def search
-    render :text => params[:query]
+    @query = params[:query]
+    @movies = []
+    if @query
+      begin
+        uri = "http://api.themoviedb.org/2.1/Movie.search/en-US/json/#{Settings.tmdb_key}/#{CGI::escape(@query)}"
+        result = Curl::Easy.perform(uri).body_str
+        unless !!(result.match(/^<h1>Not Found/)) or !!(result.match(/^\["Nothing found/))
+          @movies = JSON.parse(result)
+          unless @movies.empty?
+            @movies = @movies.map do |i|
+              hash = {
+                title: i['name'],
+                slug: i['imdb_id'],
+                mpaa_rating: i['certification']
+              }
+              hash[:poster_url] = i['posters'].first['image']['url'] if i['posters'].first
+              hash[:year] = i['released'].split(/-/).first.to_i if i['released']
+              hash
+            end
+          end
+        end
+      rescue nil
+      end
+    end
+    respond_with @movies
   end
 
   def show
