466cfa0d623c6aca1b882a938f59b0bc0b5e885d
Jacob Gillespie <me@jacobwg.com>
2012-03-13 18:58:28 -0500

    fixed rails 3 fullpath reference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::Base
   before_filter :remove_heroku
 
   def remove_heroku
-    redirect_to(request.protocol + 'checkafilm.com' + request.request_uri) if /herokuapp/.match(request.host)
+    Rails.logger.info request.methods
+    redirect_to(request.protocol + 'checkafilm.com' + request.fullpath) if /herokuapp/.match(request.host)
   end
 
 end
