6beb50ce0ad893fe81cd6ab73688384e984414d5
Jacob Gillespie <me@jacobwg.com>
2012-04-27 18:03:57 -0500

    added new thumbnail processing

--- app/uploaders/photo_asset_uploader.rb
+++ app/uploaders/photo_asset_uploader.rb
@@ -19,20 +19,20 @@ storage :fog
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/assets/defaults/poster.png"
+    "/assets/defaults/" + [version_name, "poster.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [400, 600]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [70, 110]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
