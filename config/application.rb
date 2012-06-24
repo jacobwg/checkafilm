e56904f28d0428d7b9d919fb6edc42bade906eb5
Jacob Gillespie <me@jacobwg.com>
2012-06-23 21:07:42 -0500

    hacked loader

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,13 @@
+# hacked loader
+if FileTest.exist?(Rails.root.join('.env'))
+  File.open(Rails.root.join('.env'), 'r') do |file|
+    file.each_line do |l|
+      parts = l.split('=')
+      ENV[parts.first] = parts.last
+    end
+  end
+end
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
--- config/initializers/0_loadenv.rb
+++ config/initializers/0_loadenv.rb
@@ -1,8 +1,8 @@
 if FileTest.exist?(Rails.root.join('.env'))
   File.open(Rails.root.join('.env'), 'r') do |file|
     file.each_line do |l|
-      puts l.split('=')
-      #ENV[parts.first] = parts.last
+      parts = l.split('=')
+      ENV[parts.first] = parts.last
     end
   end
 end
