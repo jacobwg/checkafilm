29f48e3d48a49a48e2adb5601705042e73228d95
Jacob Gillespie <mail@jacobwg.com>
2012-08-25 16:45:05 -0500

    Added rubyception assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Checkafilm::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( rubyception/shCore.css rubyception/shThemeDefault.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
