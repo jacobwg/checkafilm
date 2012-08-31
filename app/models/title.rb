7d7498499ecc5275b5005346bed95d278a2e7e0e
Jacob Gillespie <mail@jacobwg.com>
2012-08-31 15:41:11 -0500

    Added check for blank trailer sources

--- app/models/title.rb
+++ app/models/title.rb
@@ -235,7 +235,7 @@ class Title < ActiveRecord::Base
 
     if trailers.youtube.size > 0
       trailers.youtube.each do |t|
-        t.source.gsub!(/http:\/\/.*youtube.com\/watch\?v=/, '').gsub!(/&.*$/, '')
+        t.source.gsub!(/http:\/\/.*youtube.com\/watch\?v=/, '').gsub!(/&.*$/, '') unless t.source.nil?
         trailer = Trailer.find_by_url(t.source)
         unless trailer
           trailer = Trailer.new(url: t.source, name: t.name, title_id: self.id)
