31e1cc3a230853c45d3f6ce13ece2a2778fba5f1
Jacob Gillespie <mail@jacobwg.com>
2013-05-22 10:51:12 -0500

    Added upcoming movies

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var tmdb = require('../lib/tmdb');
 
 exports.index = function(req, res){
   tmdb.nowPlaying(function(nowPlaying) {
-    res.render('index', {title: 'Check a Film', nowPlaying: nowPlaying});
+    tmdb.upcoming(function(upcoming) {
+      res.render('index', {title: 'Check a Film', nowPlaying: nowPlaying, upcoming: upcoming, json: JSON.stringify(upcoming)});
+    });
   });
 };
